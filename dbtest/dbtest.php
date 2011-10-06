70e35e8b70170453a51c107d30b02d3202c0fbb1
Richard Russon (flatcap) <richard.russon@gmail.com>
2011-10-06 19:40:14 +0100

    use example db

---
+++ dbtest/dbtest.php
@@ -0,0 +1,26 @@
+<?php
+
+include 'db.php';
+
+date_default_timezone_set('UTC');
+
+echo "<pre>\n";
+$name = 'setter';
+
+// wake up db
+$c = db_select ($name);
+
+$time_start = microtime (true);
+
+for ($i = 0; $i < 200; $i++)
+	$c = db_select ($name);
+
+$time_end   = microtime (true);
+printf ("$name has %d entries\n", count ($c));
+
+$diff = $time_end - $time_start;
+
+printf ("elapsed = %.6f (1/%.0f) seconds\n", $diff, 1/$diff);
+
+ob_flush();
+
